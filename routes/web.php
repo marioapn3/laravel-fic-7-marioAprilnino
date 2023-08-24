057e5dfd504cc1e2c552c1c9df237c672419375e
marioapn3 <mario.aprilnino27@gmail.com>
2023-08-24 17:58:20 +0700

    pertemuan 3

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login', ['type_menu' => '']);
+});
+
+Route::middleware('auth', 'verified')->group(function () {
+    Route::get('/home', function () {
+        return view('pages.blank-page', ['type_menu' => '']);
+    })->name('home');
+});
+
+// Route::get('/login', function () {
+//     return view('auth.login', ['type_menu' => '']);
+// });
+
+// Route::get('/register', function () {
+//     return view('auth.register', ['type_menu' => '']);
+// });
+
+// Route::get('/reset', function () {
+//     return view('auth.reset', ['type_menu' => '']);
+// });
+// Route::get('/forgot', function () {
+//     return view('auth.forgot', ['type_menu' => '']);
+// });
+// Route::get('/verify', function () {
+//     return view('auth.verify', ['type_menu' => '']);
+// });
