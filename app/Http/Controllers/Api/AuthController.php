2ff9a468f0b2bad7680303bcfa5aa60c91ff22a7
marioapn3 <mario.aprilnino27@gmail.com>
2023-09-16 00:52:20 +0700

    feat : upload image api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -42,7 +42,37 @@ class AuthController extends Controller
         );
     }
 
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|string|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'user'
+        ]);
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json(
+            [
+                'jwt-token' => $token,
+                'user' => new UserResource($user),
+            ]
+        );
+    }
+
     public function logout(Request $request)
     {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'logout successfully'
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UploadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,4 +25,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
 
-Route::post('/login', [AuthController::class, 'login']);
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login',  'login');
+    Route::post('/register', 'register');
+
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
+});
+
+Route::post('image/upload', [UploadController::class, 'uploadImage',]);
+Route::post('image/upload-multiple', [UploadController::class, 'uploadMultipleImage',]);
